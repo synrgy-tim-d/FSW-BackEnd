d4dc607ee1d7f2e9ada880b28dd5d53ddd899836
Farrel Abyansyah <farrel.abyansyah@gmail.com>
2023-02-05 19:19:03 +0700

    add cloudinary & enable cors

--- app/index.js
+++ app/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
-
+require("dotenv").config();
+const cors = require("cors");
 const app = express();
-
 const router = require("./router");
+
+app.use(cors());
 app.use(router);
 
 module.exports = app;
