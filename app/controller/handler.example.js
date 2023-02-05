957f895fcf584afb42a77e459c4030879b2ff1ed
Farrel Abyansyah <farrel.abyansyah@gmail.com>
2023-02-05 19:18:22 +0700

    add handler & route example

---
+++ app/controller/handler.example.js
@@ -0,0 +1,48 @@
+const {
+  Booking,
+  BookingDetail,
+  User,
+  Kost,
+  KostRoom,
+} = require("../../models/index");
+
+module.exports = async (req, res) => {
+  try {
+    const getBooking = await Booking.findAll({
+      attributes: [
+        "booking_id",
+        ["booking_date_start", "start"],
+        "booking_date_end",
+      ],
+      include: [
+        {
+          model: BookingDetail,
+          attributes: {
+            exclude: ["booking_id", "created_at", "updated_at"],
+          },
+        },
+        {
+          model: User,
+          attributes: {
+            exclude: ["id", "created_at", "updated_at"],
+          },
+        },
+        {
+          model: Kost,
+          attributes: {
+            exclude: ["id", "created_at", "updated_at"],
+          },
+        },
+        {
+          model: KostRoom,
+          attributes: {
+            exclude: ["id", "created_at", "updated_at"],
+          },
+        },
+      ],
+    });
+    res.status(200).json(getBooking);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
--- app/router.js
+++ app/router.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
+// const { checkRole } = require("../app/controller/handler.example");
+const getBooking = require("../app/controller/handler.example");
 
 router.use(express.json());
 
 router.get("/", (req, res) => {
   res.status(200).send("Anjas");
 });
+
+router.get("/test", getBooking);
 
 module.exports = router;
